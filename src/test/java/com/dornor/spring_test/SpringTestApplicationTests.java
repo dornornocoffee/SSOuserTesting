151208d79f33490a5ca1d21ad6dad97ac4938551
dornor <suchaya_a@cmu.ac.th>
2025-02-04 15:32:53 +0700

    intial

---
+++ src/test/java/com/dornor/spring_test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dornor.spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
